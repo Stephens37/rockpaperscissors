17c8c04fe08947f8953eda4c1211aa7402d57ad0
Stephens37 <braydenstephens@protonmail.com>
2022-12-10 11:11:22 -0600

    Add player choice and ability to see result.

--- rps.js
+++ rps.js
@@ -1,7 +1,33 @@
-function getComputerChoice() {
-    const options = ["rock", "paper", "scissors"];
+function computerPlay() {
+    const options = ["ROCK", "PAPER", "SCISSORS"];
     return options[Math.floor(Math.random()*options.length)];
 }
 
-function getPlayerChoice()
-console.log(getComputerChoice())
+let getPlayerChoice = prompt("Choose rock, paper, or scissors.");
+
+let getComputerChoice = computerPlay();
+
+
+function answer(){
+    if (getPlayerChoice.toUpperCase() === "ROCK" && getComputerChoice === "SCISSORS") {
+        return "You win! Rock beats scissors."
+    }
+    else if (getPlayerChoice.toUpperCase() === "ROCK" && getComputerChoice === "PAPER") {
+        return "You lose! Paper beats rock."
+    }
+    else if (getPlayerChoice.toUpperCase() === "PAPER" && getComputerChoice === "ROCK") {
+        return "You win! Paper beats rock."
+    }
+    else if (getPlayerChoice.toUpperCase() === "PAPER" && getComputerChoice === "SCISSORS") {
+        return "You lose! Scissors beats paper."
+    }
+    else if (getPlayerChoice.toUpperCase() === "SCISSORS" && getComputerChoice === "PAPER") {
+        return "You win! Scissors beats paper."
+    }
+    else if (getPlayerChoice.toUpperCase() === "SCISSORS" && getComputerChoice === "ROCK") {
+        return "You lose! Rock beats scissors."
+    }
+    else return "Its a tie!"
+    }
+
+console.log(answer())
